6123e51236048c66a49ab1a9e3604adbe732ac97
weizhan1 <weizhan@gmail.com>
2015-02-24 20:34:46 -0800

    Update decordemo.py

--- decordemo.py
+++ decordemo.py
@@ -1,3 +1,17 @@
+>> from functools import wraps
+>>> def my_decorator(f):
+...     @wraps(f)
+...     def wrapper(*args, **kwds):
+...         print('Calling decorated function')
+...         return f(*args, **kwds)
+...     return wrapper
+...
+>>> @my_decorator
+... def example():
+...     """Docstring"""
+...     print('Called example function')
+...
+
 from decorator import my_decorator
 from _ast import Num
 
